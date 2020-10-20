7192015f2de645974337722453d5a9166198e647
남정현 <1988nam@naver.com>
2020-10-20 23:31:20 +0900

    윈도우에서 커밋

--- exam_nam/hello.py
+++ exam_nam/hello.py
@@ -1,7 +1,12 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('/Users/a1100291/PycharmProjects/pythonProject/exam_nam/chromedriver')
+# 시스템이 윈도우인 경우와 아닌 경우에 따라 driver loading 다르게 해 오는 것 구현
+# 기존 11번가 시스템은 어떤지 확인 필요
+
+#driver = webdriver.Chrome('/Users/a1100291/PycharmProjects/pythonProject/exam_nam/chromedriver')
+driver = webdriver.Chrome('/Users/남정현/PycharmProjects/pythonProject/exam_nam/chromedriver_win')
+
 # 웹드라이버 실행 경로 chromedriver는 폴더가 아니라 파일명입니다.
 driver.get('http://www.google.com/');   	# 구글에 접속
 time.sleep(2)					# 2초간 동작하는 걸 봅시다
