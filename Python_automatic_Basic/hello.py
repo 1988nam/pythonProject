121d40176da0ca765408b5f293f695f4b413c5c7
남정현 <1988nam@naver.com>
2020-11-02 16:33:14 +0900

    Tuple

--- Python_automatic_Basic/hello.py
+++ Python_automatic_Basic/hello.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 driver = webdriver.Chrome('/Users/남정현/PycharmProjects/pythonProject/Python_automatic_Basic/chromedriver_win')
 
 # 웹드라이버 실행 경로 chromedriver는 폴더가 아니라 파일명입니다.
-driver.get('http://www.google.com/');   	# 구글에 접속
+driver.get('http://www.11st.co.kr/');   	# 구글에 접속
 time.sleep(2)					# 2초간 동작하는 걸 봅시다
 search_box = driver.find_element_by_name('q')   # element name이 q인 곳을 찾아
 search_box.send_keys('ChromeDriver')		# 키워드를 입력하고
---
+++ python_basic/chapter03_03.py
@@ -0,0 +1,16 @@
+
+a = []
+print(type(a))
+b = list()
+c = [70, 75, 80, 85] #len
+d = [1000, 10000, 'Ace', 'Base', 'Captine']
+e = [1000, 10000, ['Ace', 'Base', 'Captine']]
+f = [21.42, 'foobar',3,4,False,3.14159]
+
+#인덱싱
+print('>>>>>')
+print('d - ', type(d),d)
+print('d - ', d[1])
+print('d - ', d[0]+d[1]+d[1])
+print('d - ', d[-1])
+print('e - ', e[-1][1])
