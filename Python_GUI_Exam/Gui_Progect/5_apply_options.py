69247eda94559169d71a9898d714161590d5e370
남정현 <1988nam@naver.com>
2021-05-09 23:28:46 +0900

    Python GUI Exam create apply options

--- Python_GUI_Exam/Gui_Progect/5_apply_options.py
+++ Python_GUI_Exam/Gui_Progect/5_apply_options.py
@@ -8,18 +8,19 @@ from PIL import Image
 root = Tk()
 root.title("Nado GUI")
 
+
 # 파일 추가
 def add_file():
-    files = filedialog.askopenfilenames(title='이미지 파일을 선택하세요',\
-        filetypes=(('PNG 파일','*.PNG'),('모든 파일','*.*')),\
-        initialdir='c:/download')
+    files = filedialog.askopenfilenames(title='이미지 파일을 선택하세요', \
+                                        filetypes=(('PNG 파일', '*.PNG'), ('모든 파일', '*.*')), \
+                                        initialdir='c:/download')
 
-    #사용자가 선택한 파일 목록
+    # 사용자가 선택한 파일 목록
     for file in files:
         list_file.insert(END, file)
 
-def del_file():
 
+def del_file():
     for index in reversed(list_file.curselection()):
         list_file.delete((index))
 
@@ -29,27 +30,61 @@ def browser_dest_path():
     folder_selected = filedialog.askdirectory()
     if folder_selected == '':
         return
-    txt_dest_path.delete(0,END)
-    txt_dest_path.insert(0,folder_selected)
+    txt_dest_path.delete(0, END)
+    txt_dest_path.insert(0, folder_selected)
+
 
 # 이미지 통합
 
 def merge_image():
-    #print(list_file.get(0,END))
-    images = [Image.open(x) for x in list_file.get(0,END)]
-    # size = size[0] : width, size[1] : height
-    # widths = [x.size[0] for x in images]
-    # heights = [x.size[1] for x in images]
+    # print('가로넓이 :', cmb_width.get())
+    # print('간격 :', cmb_space.get())
+    # print('포맷 : ', cmb_format.get())
 
-    widths, heights = zip(*(x.size for x in images))
+    # 가로 넓이
+    img_width = cmb_width.get()
+    if img_width == "원본유지":
+        img_width = -1
+    else:
+        img_width = int(img_width)
+
+    # 간격
+    img_space = cmb_space.get()
+    if img_space == "좁게":
+        img_space == 30
+    elif img_space == "보통":
+        img_space == 60
+    elif img_space == "넓게":
+        img_space == 90
+    else:
+        img_space == 0
+
+    # 포맷
+    img_format = cmb_format.get().lower()
+
+
+    images = [Image.open(x) for x in list_file.get(0, END)]
+
+    # 이미지 사이즈 리스트에 넣어서 하나씩 처리
+    image_sizes = []
+    if img_width > -1:
+        image_sizes = [(int(img_width),int(img_width * x.size[1] / x.size[0])) for x in images]
+    else:
+        image_sizes = [(x.size[0], x.size[1]) for x in images]
+
+    # 계산식
+    # 100 * 60 이미지가 있음 -> width 를 80으로 줄이면 height 는 ?
+    # 원본 width : 원본 height -> 변경 width :  height
+
+    widths, heights = zip(*(image_sizes))
 
     max_width, total_height = max(widths), sum(heights)
     print(max(widths))
     print(total_height)
 
-    #스케치북 준비
-    result_img = Image.new('RGB',(max_width, total_height),(255,255,255))
-    y_offset = 0 # y 위치 정보
+    # 스케치북 준비
+    result_img = Image.new('RGB', (max_width, total_height), (255, 255, 255))
+    y_offset = 0  # y 위치 정보
     # for img in images:
     #     result_img.paste(img,(0,y_offset))
     #     y_offset += img.size[1]
@@ -58,37 +93,38 @@ def merge_image():
         result_img.paste(img, (0, y_offset))
         y_offset += img.size[1]
 
-        progress = (idx+1) / len(images) * 100 # 실제 % 정보를 계산
+        progress = (idx + 1) / len(images) * 100  # 실제 % 정보를 계산
         p_var.set(progress)
         progress_bar.update()
 
-    dest_path = os.path.join(txt_dest_path.get(),'nam_photo.jpg')
+    dest_path = os.path.join(txt_dest_path.get(), 'nam_photo.jpg')
     result_img.save(dest_path)
-    msgbox.showinfo('알림','작업 완료')
+    msgbox.showinfo('알림', '작업 완료')
 
 
 # 시작
 
 def start():
     # 각 옵션들 값을 확인
-    print('가로넓이 :', cmb_width.get())
-    print('간격 :', cmb_space.get())
-    print('포맷 : ', cmb_format.get())
+    # print('가로넓이 :', cmb_width.get())
+    # print('간격 :', cmb_space.get())
+    # print('포맷 : ', cmb_format.get())
 
     # 파일 목록 확인
 
     if list_file.size() == 0:
-        msgbox.showwarning('경고','이미지 파일을 추가 하세요')
+        msgbox.showwarning('경고', '이미지 파일을 추가 하세요')
         return
 
     # 저장 경로 확인
 
     if len(txt_dest_path.get()) == 0:
-        msgbox.showwarning('경고','저장 경로를 선택하세요')
+        msgbox.showwarning('경고', '저장 경로를 선택하세요')
         return
 
-    #이미지 통합 작업
+    # 이미지 통합 작업
     merge_image()
+
 
 # 파일 프레임 (파일 추가, 선택 삭제)
 file_frame = Frame(root)
@@ -108,7 +144,7 @@ list_frame.pack(fill='both', padx=5, pady=5)
 scrollbar = Scrollbar(list_frame)
 scrollbar.pack(side='right', fill='y')
 
-list_file = Listbox(list_frame, selectmode='extended', height=15, yscrollcommand = scrollbar.set)
+list_file = Listbox(list_frame, selectmode='extended', height=15, yscrollcommand=scrollbar.set)
 list_file.pack(side='left', fill='both', expand='True')
 scrollbar.config(command=list_file.yview)
 
@@ -132,7 +168,7 @@ lbl_width = Label(frame_option, text='가로 넓이', width=8)
 lbl_width.pack(side='left')
 
 # 가로 넓이 콤보
-opt_width = ['원본유지','1024','800','640']
+opt_width = ['원본유지', '1024', '800', '640']
 cmb_width = ttk.Combobox(frame_option, state='readonly', values=opt_width, width=10)
 cmb_width.current(0)
 cmb_width.pack(side='left')
@@ -143,11 +179,10 @@ lbl_space = Label(frame_option, text='간격', width=8)
 lbl_space.pack(side='left', padx=5, pady=5)
 
 # 간격 옵션 콤보
-opt_space = ['없음','좁게','보통','넓게']
+opt_space = ['없음', '좁게', '보통', '넓게']
 cmb_space = ttk.Combobox(frame_option, state='readonly', values=opt_space, width=10)
 cmb_space.current(0)
 cmb_space.pack(side='left', padx=5, pady=5)
-
 
 # 파일 포맷 옵션
 # 파일 포맷 레이블
@@ -155,7 +190,7 @@ lbl_format = Label(frame_option, text='간격', width=8)
 lbl_format.pack(side='left', padx=5, pady=5)
 
 # 파일 포맷 콤보
-opt_format = ['PNG','JPG','BMP']
+opt_format = ['PNG', 'JPG', 'BMP']
 cmb_format = ttk.Combobox(frame_option, state='readonly', values=opt_format, width=10)
 cmb_format.current(0)
 cmb_format.pack(side='left', padx=5, pady=5)
@@ -172,14 +207,11 @@ progress_bar.pack(fill='x', padx=5, pady=5)
 frame_run = LabelFrame(root)
 frame_run.pack(fill='x', padx=5, pady=5)
 
-
 btn_close = Button(frame_run, text='닫기', padx=5, pady=5, width=12, command=root.quit)
 btn_close.pack(side='right', padx=5, pady=5)
 
 btn_start = Button(frame_run, text='시작', padx=5, pady=5, width=12, command=start)
 btn_start.pack(side='right', padx=5, pady=5)
-
-
 
 root.resizable(False, False)
 root.mainloop()
