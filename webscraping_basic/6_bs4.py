10640d680828177329062fd2ef0157a79b74e4f5
남정현 <1988nam@naver.com>
2021-05-16 23:46:07 +0900

    webscarping exam 3

--- webscraping_basic/6_bs4.py
+++ webscraping_basic/6_bs4.py
@@ -16,5 +16,19 @@ soup = BeautifulSoup(res.text,"lxml")
 # print(soup.find("a",attrs={"class":"Nbtn_upload"}))
 # print(soup.find("li",attrs={"class":"rank01"}))
 
-rank1 = soup.find("li",attrs={"class":"rank01"})
-print(rank1.a)
+# rank1 = soup.find("li",attrs={"class":"rank01"})
+# # print(rank1.a.get_text())
+# # print(rank1.next_sibling)
+# # print(rank1.next_sibling.next_sibling)
+#
+# rank2 = rank1.find_next_sibling("li")
+# print(rank2.a.get_text())
+# rank3 = rank2.find_next_sibling("li")
+# print(rank3.a.get_text())
+#
+# rank1.find_next_siblings("li")
+# print(rank1.find_next_siblings("li"))
+
+webtoon = soup.find("a", text="소녀의 세계-2부 55화")
+
+print(webtoon)
