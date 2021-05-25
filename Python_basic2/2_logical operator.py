3b803daca663d57cefde4e5752849b77429ffb16
남정현 <1988nam@naver.com>
2021-05-25 23:45:48 +0900

    기초문법 복습_3

--- Python_basic2/2_logical operator.py
+++ Python_basic2/2_logical operator.py
@@ -1,9 +1,26 @@
-user_id = input('id')
-user_pwd = input('password?')
+# user_id = input('id')
+# user_pwd = input('password?')
+#
+# if user_id =='nam' and user_pwd =='1':
+#     print('hello master')
+# elif user_id =='nam2' and user_pwd =='2':
+#     print('hello master2')
+# else:
+#     print('who are you')
+#
+# 생활코딩 문서 보는 법, 리스트 강의 수강
+# https://youtu.be/SUOV_AYMk8o
 
-if user_id =='nam' and user_pwd =='1':
-    print('hello master')
-elif user_id =='nam2' and user_pwd =='2':
-    print('hello master2')
-else:
-    print('who are you')
+s = [1,'four',9,16,25]
+print(s)
+print(s[1])
+print(len(s))
+
+s[1] = 4
+# s.remove(s[1])
+del s[1]
+s.append(100)
+print(s)
+#
+# 다음 공부 강의
+# https://youtu.be/M9htx1hZfK4
