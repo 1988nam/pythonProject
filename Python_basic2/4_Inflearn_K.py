db95be1635905366cbc8151d700ebec5e9e68ded
남정현 <1988nam@naver.com>
2021-06-04 00:26:32 +0900

    인프런 알고리즘 K 번째 약수 풀이_1

--- Python_basic2/4_Inflearn_K.py
+++ Python_basic2/4_Inflearn_K.py
@@ -6,5 +6,33 @@
 # 1 <= K <= N
 
 
-n = input("N 을 입력하시오")
-print(n)
+n = input("N 을 입력하시오: ")
+k = input("K 를 입력하시오: ")
+
+n = int(n)
+k = int(k)
+
+# if n<1:
+#     print("n은 1이상을 입력하세요")
+# elif n>10000:
+#     print("n은 10000 이하를 입력하세요")
+# else:
+#     print("입력한 N은",n)
+#
+# if k < 1:
+#     print("k는 1이상을 입력하세요")
+# elif k > n:
+#     print("k는 n이하를 입력하세요")
+# else:
+#     print("입력한 K는", k)
+
+"n 의 6의 약수 구하기"
+cnt=0
+for i in range(1,n+1):
+    if n%i==0:
+        cnt+=1
+    if cnt==k:
+        print(i)
+        break
+else:
+    print(-1)
