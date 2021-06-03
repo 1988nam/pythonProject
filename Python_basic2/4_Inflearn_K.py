3db2b8859ea4cdde735b971e23175594debeff8b
남정현 <1988nam@naver.com>
2021-06-04 00:00:15 +0900

    인프런 알고리즘 K 번째 약수 풀이_1

---
+++ Python_basic2/4_Inflearn_K.py
@@ -0,0 +1,10 @@
+# import sys
+# sys.stdin=open("input.txt","rt")
+
+# 두 개의 자연수 N K 가 주어졌을 때 N의 약수들 중 K 번째로 작은 수를 출력하는 프로그램을 작성 하시오
+# 1 <= N <= 10,000
+# 1 <= K <= N
+
+
+n = input("N 을 입력하시오")
+print(n)
