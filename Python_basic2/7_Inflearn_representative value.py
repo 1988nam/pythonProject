bb28f40c55a9e360ed85ba6616a7d30e735c4daa
남정현 <1988nam@naver.com>
2021-06-16 22:22:49 +0900

    인프런 알고리즘 대표값 알고리즘

---
+++ Python_basic2/7_Inflearn_representative value.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin=open("in7.txt","rt")
+
+n=int(input())
+a=list(map(int, input().split()))
+
+# 15
+# 12 34 17 6 11 15 27 42 39 31 25 36 35 25 17
+
+min=2147000000
+ave = round(sum(a)/n)
+for idx, x in enumerate(a):
+    tmp = abs(x-ave)
+    if tmp < min:
+        min = tmp
+        score = x
+        res=idx+1
+    elif tmp == min:
+        if x>score:
+            score = x
+            res=idx+1
